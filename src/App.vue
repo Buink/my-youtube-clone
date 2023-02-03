3b6326f858b8a74bf82b9e5625f2db0059481aaf
Buink <armatasplay@gmail.com>
2023-02-03 19:59:52 +0200

    v1.0

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <TheHeader />
+  <TheSideBarSmall />
+  <TheSidebar />
+  <TheSidebarMobile />
+  <TheCategories />
+  <TheVideos />
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheSideBarSmall from "./components/TheSideBarSmall.vue";
+import TheSidebar from "./components/TheSidebar.vue";
+import TheSidebarMobile from "./components/TheSidebarMobile.vue";
+import TheCategories from "./components/TheCategories.vue";
+import TheVideos from "./components/TheVideos.vue";
+
+export default {
+  name: "App",
+  components: {
+    TheHeader, TheSideBarSmall, TheSidebar, TheSidebarMobile, TheCategories, TheVideos
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
