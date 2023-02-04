cfd2b325ffec95c7c177a97ee6e420db737ae9f8
Buink <armatasplay@gmail.com>
2023-02-05 00:12:46 +0200

    v1.0 Creating vue components

--- src/components/TheVideos.vue
+++ src/components/TheVideos.vue
@@ -3,7 +3,7 @@
     <div
       class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-screen-2xl m-auto"
     >
-      <VideoItem v-for="i in 20" :key="i" :index="i"/>
+      <VideoItem v-for="i in 20" :key="i" :index="i" />
     </div>
   </main>
 </template>
@@ -15,7 +15,6 @@ export default {
   components: {
     VideoItem,
   },
-
 };
 </script>
 
